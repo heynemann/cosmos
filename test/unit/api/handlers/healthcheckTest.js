19cf2f74628f597415c831c252105085b1d835ac
Bernardo Heynemann <heynemann@gmail.com>
2016-10-04 00:52:10 -0300

    App tests

--- test/unit/api/handlers/healthcheckTest.js
+++ test/unit/api/handlers/healthcheckTest.js
@@ -1,25 +1,17 @@
-import * as sap from 'supertest-as-promised'
-import config from 'config'
-
-import CosmosApp from '../../../../src/api/app'
 import { expect } from '../../common'
 
-describe('Healthcheck Handler', () => {
-  beforeEach(async function () {
-    this.app = new CosmosApp(config)
-    await this.app.initializeApp()
-    this.request = sap.agent(this.app.app.listen())
-  })
+describe('Handlers', () => {
+  describe('Healthcheck Handler', () => {
+    it('should return 200 if all services up', async function () {
+      const res = await this.request.get('/healthcheck')
+      expect(res.status).to.equal(200)
 
-  it('should return 200 if all services up', async function () {
-    const res = await this.request.get('/healthcheck')
-    expect(res.status).to.equal(200)
+      const body = res.text
+      expect(body).not.to.equal('')
 
-    const body = res.text
-    expect(body).not.to.equal('')
-
-    const result = JSON.parse(body)
-    expect(result.mongo).not.to.equal(null)
-    expect(result.mongo.up).to.equal(true)
+      const result = JSON.parse(body)
+      expect(result.mongo).not.to.equal(null)
+      expect(result.mongo.up).to.equal(true)
+    })
   })
 })
--- test/unit/common.js
+++ test/unit/common.js
@@ -1,4 +1,9 @@
+import * as sap from 'supertest-as-promised'
+import config from 'config'
+
+import CosmosApp from '../../src/api/app'
 import { expect as exp, chai as chaiMod } from '../common'
+
 
 const initializeTests = () => {
 }
@@ -7,3 +12,11 @@ export const expect = exp
 export const chai = chaiMod
 
 initializeTests()
+
+beforeEach(async function () {
+  this.app = new CosmosApp(config)
+  await this.app.initializeApp()
+
+  // From http://stackoverflow.com/questions/33369389/how-can-i-use-es2016-es7-async-await-in-my-acceptance-tests-for-a-koa-js-app
+  this.request = sap.agent(this.app.app.listen())
+})
